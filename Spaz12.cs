b6749566a7ca8ff969235881c477677685b10375
Devon Plackard <60117133+dplackard@users.noreply.github.com>
2020-02-22 15:27:12 -0600

    Add files via upload

---
+++ Spaz12.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WW3_Library
+{
+    public class Spaz12 : Weapons
+    {
+        public int Spaz12Min { get; set; }
+        public int Spaz12Max { get; set; }
+        public int Spaz12Accuracy { get; set; }
+        public int Spaz12RangeMin { get; set; }
+        public int Spaz12RangeMax { get; set; }
+    }
+}
---
+++ Weapons.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WW3_Library
+{
+    public class Weapons
+    {
+        private int _minDamage;
+
+        public string Name { get; set; }
+        public int MaxDamage { get; set; }
+        public int Accuracy { get; set; }
+        public int MinRange { get; set; }
+        public int MaxRange { get; set; }
+        public int HeadshotDamage { get; set; }
+        public int MinDamage
+        {
+            get { return _minDamage; }
+            set
+            {
+                _minDamage = value <= MaxDamage && value > 0 ? value : 1;
+            }
+        }
+        public Weapons(string name, int accuracy, int minRange, int maxRange, int headshotDamage, int maxDamage, int minDamage)
+        {
+            Name = name;
+            Accuracy = accuracy;
+            MinRange = minRange;
+            MaxRange = maxRange;
+        }
+    }
+}
